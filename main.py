a310d92a0db78aac08572f2ecc63fbb1f5ba1bbd
stepanusimnuel <nuel597@gmail.com>
2025-05-19 10:31:28 +0700

    create and read workouts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect, request, flash, abort
+from .models import User, Workout
+from . import db
 from flask_login import login_required, current_user
 
 # initialize main as part of application
@@ -12,3 +14,29 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/new')
+@login_required
+def new_workout():
+    return render_template('create_workout.html')
+
+@main.route('/new', methods=['POST'])
+@login_required
+def new_workout_post():
+    pushups = request.form.get('pushups')
+    comment = request.form.get('comment')
+
+    workout = Workout(pushups=pushups, comment=comment, author=current_user)
+    db.session.add(workout)
+    db.session.commit()
+
+    flash('your workout has been added!')
+
+    return redirect(url_for('main.user_workouts'))
+
+@main.route('/all')
+@login_required
+def user_workouts():
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+    workouts = user.workouts
+    return render_template('all_workouts.html', workouts=workouts, user=user)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin # objek user punya is_authenticated, is_active, is_anonymus, get_id()
+from datetime import datetime
 
 # create database cli
 # from pushup_logger import db, create_app
@@ -14,3 +15,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    # 1 user punya banyak workout
+    # backref agar bisa mengakses dari Workout ke User menggunakan .author
+    workouts = db.relationship('Workout', backref='author', lazy=True)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pushups = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    comment = db.Column(db.Text, nullable=False)
+    # setiap workout (pushup) dimiliki oleh 1 user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
