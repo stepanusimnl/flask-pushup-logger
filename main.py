ca9697cd89d47c0db6d62a616af338fb92ae7737
stepanusimnuel <nuel597@gmail.com>
2025-05-19 14:05:57 +0700

    pagination

--- main.py
+++ main.py
@@ -37,8 +37,10 @@ def new_workout_post():
 @main.route('/all')
 @login_required
 def user_workouts():
+    page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(email=current_user.email).first_or_404()
-    workouts = user.workouts
+    workouts = Workout.query.filter_by(author=user).paginate(page=page, per_page=2)
+    
     return render_template('all_workouts.html', workouts=workouts, user=user)
 
 @main.route('/workout/<int:workout_id>/update', methods=['GET', 'POST'])
@@ -61,4 +63,5 @@ def delete_workout(workout_id):
     workout = Workout.query.get_or_404(workout_id)
     db.session.delete(workout)
     db.session.commit()
+    flash('your workout has been deleted!')
     return redirect(url_for('main.user_workouts'))
